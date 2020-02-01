6362efd448592d0ebaaf5a09bbc0db4813057bb6
Saksham Tyagi <saksham.tyagi@mapmyindia.com>
2020-02-01 16:29:14 +0530

    Remove Some unused codes

--- app/src/main/java/com/mapmyindia/sdk/demo/MapApplication.java
+++ app/src/main/java/com/mapmyindia/sdk/demo/MapApplication.java
@@ -3,7 +3,6 @@ package com.mapmyindia.sdk.demo;
 import android.app.Application;
 
 import com.mapbox.mapboxsdk.MapmyIndia;
-import com.mapbox.mapboxsdk.module.http.HttpRequestUtil;
 import com.mmi.services.account.MapmyIndiaAccountManager;
 
 /**
@@ -22,31 +21,6 @@ public class MapApplication extends Application {
         MapmyIndiaAccountManager.getInstance().setAtlasClientSecret(getAtlasClientSecret());
         MapmyIndiaAccountManager.getInstance().setAtlasGrantType(getAtlasGrantType());
         MapmyIndia.getInstance(this);
-      HttpRequestUtil.setLogEnabled(true);
-      HttpRequestUtil.setPrintRequestUrlOnFailure(true);
     }
-
-
-  public String getAtlasClientId() {
-    return "SzvSlV-lKpZYFvG2Z_6NKQ0RzHFEPAMQNVaeHCzWARnG1U3bMVuXAToRv6J1hkGwegy3xeUIuoAvpwNUSBC8e3Bz5WyThCwo";
-  }
-
-  public String getAtlasClientSecret() {
-    return "QkUOZ5yFbmuIhOzVsd_dE9BFy_KWpEnyMSIi962k0r32GQzf6JA6Ik2tC9k9GDtUPFHs_zv78X6mlVjnCmTkKd-EtxmN_onUe9WaSYfWVUQ=";
-  }
-
-
-  public String getAtlasGrantType() {
-    return "client_credentials";
-  }
-
-  public String getMapSDKKey() {
-    return "dd59538de9aa0f97ba171a1d4cb293bc";
-  }
-
-  public String getRestAPIKey() {
-    return "a40738c26b1e79b0c73e887628a2d75f";
-  }
-
 
 }
